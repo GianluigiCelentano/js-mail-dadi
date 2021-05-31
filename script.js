c90c40e85486d4e0a47fb1729a6bdf0479478bb8
GianluigiCelentano <cgianluigi96@gmail.com>
2021-05-31 17:01:17 +0200

    js-mail-dadi

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var askMail = prompt("Inserisci la tua mail");
 var databaseMail = ["ottavio@gmail.com", "davide@gmail.com", "alfredo@gmail.com"];
-if (askMail === databaseMail) {
-    document.getElementById("welcome").innerHTML = "benvenuto"
+var check = false
+for (var i = 0; i < databaseMail.length; i++){
+    if(askMail === databaseMail[i]){
+        check = true;
+    }
+}
+if(check === true) {
+    alert ("Puoi giocare")
 } else {
-    prompt("Mail non presente")
+    alert ("mail non presente")
+}
+if(check === true) {
+    var computer = parseInt[1,2,3,4,5,6]
+    var utent = parseInt[1,2,3,4,5,6]
+    var randomComputer = computer[Math.floor(Math.random()*computer.length)]
+    var randomUtent = utent[Math.floor(Math.random()*utent.length)]
+    document.getElementById("computer").innerHTML += randomComputer
+    document.getElementById("utent").innerHTML += randomUtent
 }
